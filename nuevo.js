4304db87ed29b50d3e87be4d79c3a594772209d1
Luizz360 <65743912+Luizz360@users.noreply.github.com>
2020-06-28 19:42:47 -0500

    Delete nuevo.js

--- nuevo.js
+++
@@ -1,9 +0,0 @@
-
-    $(document).ready(function(){
-        $("#boton1").click(function(event){
-           $("logo").load("dos.html");
-           $("#boton2").click(function(event){
-        $("numero").load("dos.html");
-         });
-        });
-    });      
